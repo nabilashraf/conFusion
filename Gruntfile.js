64b72e679317fa9dca0c94505335f9071d09d2ba
Nabil Ashraf <nabeelashraf.2008@gmail.com>
2017-10-21 07:01:46 +0500

    Grunt Part 1

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+'use strict'
+
+module.exports = function(grunt) {
+	
+	require('time-grunt')(grunt);
+	require('jit-grunt')(grunt);
+	grunt.initConfig({ 
+		sass: { 
+			dist: {
+				files: {
+					'css/styles.css':'css/styles.scss'
+				}
+			}
+		},
+		watch: {
+			files: 'css/*.scss',
+			tasks: ['sass']
+		},
+
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src: [
+						'css/*.css','*.html,js/*js'
+					]
+				},
+				options: {
+					watchTask: true,
+					server: {
+						baseDir: './'
+					}
+				}
+			}
+		}
+	});
+	grunt.registerTask('css',['sass']);
+	grunt.registerTask('default',['browserSync','watch']);
+}
