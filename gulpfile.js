7476d6b0955645b979c1d520a22e847c6c7af9ed
Nabil Ashraf <nabeelashraf.2008@gmail.com>
2017-10-29 00:34:48 +0500

    Gulp Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@
 
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
-  browserSync = require('browser-sync');
+  del = require('del'),
+  browserSync = require('browser-sync'),
+  imagemin = require('gulp-imagemin'),
+  uglify = require('gulp-uglify'),
+  usemin = require('gulp-usemin'),
+  rev = require('gulp-rev'),
+  cleanCss = require('gulp-clean-css'),
+  flatmap = require('gulp-flatmap'),
+  htmlmin = require('gulp-htmlmin');
 
 
 gulp.task('sass', function () {
@@ -34,4 +42,48 @@ gulp.task('browser-sync', function () {
 // Default task
 gulp.task('default', ['browser-sync'], function () {
   gulp.start('sass:watch');
+});
+
+// Clean
+gulp.task('clean', function () {
+  return del(['dist']);
+});
+
+gulp.task('copyfonts', function () {
+  gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function () {
+  return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function () {
+  return gulp.src('./*.html')
+    .pipe(flatmap(function (stream, file) {
+      return stream
+        .pipe(usemin({
+          css: [rev()],
+          html: [function () {
+            return htmlmin({
+              collapseWhitespace: true
+            })
+          }],
+          js: [uglify(), rev()],
+          inlinejs: [uglify()],
+          inlinecss: [cleanCss(), 'concat']
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', ['clean'], function () {
+  gulp.start('copyfonts', 'imagemin', 'usemin');
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,30 @@
 $(document).ready(function () {
 
-        $('#mycarousel').carousel({
-          interval: 2000
-        });
-        $('#carousel-button').click(function () {
-          if ($("#carousel-button").children("span").hasClass("fa-pause")) {
-            $('#mycarousel').carousel('pause');
-            $("#carousel-button").children("span").removeClass("fa-pause");
-            $("#carousel-button").children("span").addClass("fa-play");
-          } else if ($("#carousel-button").children("span").hasClass("fa-play")) {
-            $('#mycarousel').carousel('cycle');
-            $("#carousel-button").children("span").removeClass("fa-play");
-            $("#carousel-button").children("span").addClass("fa-pause");
-          }
-        });
-      });
+  $('#mycarousel').carousel({
+    interval: 2000
+  });
+  $('#carousel-button').click(function () {
+    if ($("#carousel-button").children("span").hasClass("fa-pause")) {
+      $('#mycarousel').carousel('pause');
+      $("#carousel-button").children("span").removeClass("fa-pause");
+      $("#carousel-button").children("span").addClass("fa-play");
+    } else if ($("#carousel-button").children("span").hasClass("fa-play")) {
+      $('#mycarousel').carousel('cycle');
+      $("#carousel-button").children("span").removeClass("fa-play");
+      $("#carousel-button").children("span").addClass("fa-pause");
+    }
+  });
+
+  $("#loginBtn").click(function () {
+    $("#loginModal").modal({
+      show: true
+    });
+  });
+
+  $("#reserveBtn").click(function () {
+    $("#reserveModal").modal({
+      show: true
+    });
+  });
+
+});
